0b3b0e1fd35b3887873a6e565bf50b7bd39850b4
이하영 <lhy0530@khu.ac.kr>
2021-01-07 06:13:35 +0900

    [10869]사칙연산

---
+++ 10869.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(void){
+    int nums[2];
+    cin>>nums[0]>>nums[1];
+    cout<<nums[0]+nums[1]<<endl;
+    cout<<nums[0]-nums[1]<<endl;
+    cout<<nums[0]*nums[1]<<endl;
+    cout<<nums[0]/nums[1]<<endl;
+    cout<<nums[0]%nums[1];
+}
