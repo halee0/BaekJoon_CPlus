5e9104c282d016cf622dbfaa44fc8c8cda3df455
이하영 <waffle0530@naver.com>
2021-01-08 04:51:40 +0900

    [2439]별 찍기 -2

---
+++ 2439.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(void){
+    int n;
+    cin>>n;
+    for (int i=1; i<n+1; i++){
+        for (int j=n; j>i; j--){
+            cout<<" ";
+        }
+        for (int k=0; k<i;k++){
+            cout<<"*";
+        }
+        cout<<"\n";
+    }
+}
